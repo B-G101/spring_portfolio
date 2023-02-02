18102222acc4adfd70185a4303cede2ce9e83958
CalissaT <calissat@gmail.com>
2023-02-02 10:57:18 -0800

    files

--- src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
+++ src/main/java/com/nighthawk/spring_portfolio/mvc/ModelInit.java
@@ -1,32 +1,40 @@
-package com.nighthawk.spring_portfolio.mvc;
+package com.nighthawk.csa.mvc.database;
 
+import com.nighthawk.csa.mvc.database.note.NoteJpaRepository;
+import com.nighthawk.csa.mvc.database.role.Role;
+import com.nighthawk.csa.mvc.database.role.RoleJpaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import com.nighthawk.spring_portfolio.mvc.jokes.Jokes;
-import com.nighthawk.spring_portfolio.mvc.jokes.JokesJpaRepository;
-
-import java.util.List;
 
 @Component // Scans Application for ModelInit Bean, this detects CommandLineRunner
-public class ModelInit {  
-    @Autowired JokesJpaRepository repository;
+public class ModelInit {
+    // Inject repositories
+    @Autowired NoteJpaRepository noteJpaRepository;
+    @Autowired RoleJpaRepository roleJpaRepository;
+    @Autowired ModelRepository modelRepository;
 
     @Bean
     CommandLineRunner run() {  // The run() method will be executed after the application starts
         return args -> {
+            // Fail safe data validations
 
-            // Joke database is populated with starting jokes
-            String[] jokesArray = Jokes.init();
-            for (String joke : jokesArray) {
-                List<Jokes> test = repository.findByJokeIgnoreCase(joke);  // JPA lookup
-                if (test.size() == 0)
-                    repository.save(new Jokes(null, joke, 0, 0)); //JPA save
+            // make sure Role database is populated with defaults
+            String[] roles = {"ROLE_STUDENT", "ROLE_TEACHER", "ROLE_ADMIN", "ROLE_TESTER"};
+            for (String role : roles) {
+                if (roleJpaRepository.findByName(role) == null)
+                    roleJpaRepository.save(new Role(null, role));
             }
+
+            // make sure every record added has a Default encrypted password and ROLE_STUDENT
+            modelRepository.defaults("123querty", "ROLE_STUDENT");
+
+            // make sure privileged roles exist for Teacher
+            modelRepository.addRoleToPerson("jmort1021@gmail.com", "ROLE_TEACHER");
+            modelRepository.addRoleToPerson("jmort1021@gmail.com", "ROLE_ADMIN");
+
 
         };
     }
 }
-
